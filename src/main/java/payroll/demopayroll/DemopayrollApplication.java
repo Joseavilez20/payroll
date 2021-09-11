edda42163f9e06ff9ab1e5b697025a54f05f867b
Jose Antonio Aviles Pacheco <joseok56@gmail.com>
2021-09-10 21:45:28 -0500

    defined an Employee in our system

---
+++ src/main/java/payroll/demopayroll/DemopayrollApplication.java
@@ -0,0 +1,13 @@
+package payroll.demopayroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemopayrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemopayrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/payroll/demopayroll/DemopayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package payroll.demopayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemopayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
